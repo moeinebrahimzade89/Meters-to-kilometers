2f9311c9d11c72b405088fbe819140ef62415828
Moein Ebrahimzade <moein.ebrahimzade.89@gmail.com>
2026-07-13 15:36:57 +0330

    Update Meters to kilometers.py
    Add kilometer to meter mode

--- Meters to kilometers.py
+++ Meters to kilometers.py
@@ -1,5 +1,11 @@
+print("Select Convert. 1.Meters to kilometers  2.Kilometer to meter")
+Choose = input("Choose:")
 try:
-    meter = float(input("Enter length in meters: "))
-    print(f"Kilometer: {meter / 1000}")
+    meter = float(input("meter:"))
 except ValueError:
     print("Error: The entered value is not a valid number.")
+
+if Choose == '1':
+    print("Kilometer:",meter / 1000)
+elif Choose == '2':
+    print("meter:",meter * 1000)
